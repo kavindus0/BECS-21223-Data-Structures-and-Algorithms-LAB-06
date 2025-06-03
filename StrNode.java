7179b55bee9c3ceef29143fdd35c6927fdd55733
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-06-03 16:09:34 +0530

    Create StrNode.java

---
+++ StrNode.java
@@ -0,0 +1,12 @@
+
+
+class StrNode {
+    String data;
+    StrNode left, right;
+
+    public StrNode(String data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
